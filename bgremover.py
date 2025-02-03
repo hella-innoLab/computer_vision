22d2286036b3bbd4c86fa2d5ed173b92635464ca
Your Full Name <your.email@example.com>
2025-02-03 11:12:20 +0100

    basic bg-remover using opencv

---
+++ bgremover.py
@@ -0,0 +1,92 @@
+import cv2
+import numpy as np
+
+from matplotlib import pyplot as plt
+ 
+
+def showimage(myimage):
+    if (myimage.ndim>2):  #This only applies to RGB or RGBA images (e.g. not to Black and White images)
+        myimage = myimage[:,:,::-1] #OpenCV follows BGR order, while matplotlib likely follows RGB order
+         
+    fig, ax = plt.subplots(figsize=[10,10])
+    ax.imshow(myimage, cmap = 'gray', interpolation = 'bicubic')
+    plt.xticks([]), plt.yticks([])  # to hide tick values on X and Y axis
+    plt.show()
+
+
+def bgremove1(myimage):
+ 
+    # Blur to image to reduce noise
+    myimage = cv2.GaussianBlur(myimage,(5,5), 0)
+ 
+    # We bin the pixels. Result will be a value 1..5
+    bins=np.array([0,51,102,153,204,255])
+    myimage[:,:,:] = np.digitize(myimage[:,:,:],bins,right=True)*51
+ 
+    # Create single channel greyscale for thresholding
+    myimage_grey = cv2.cvtColor(myimage, cv2.COLOR_BGR2GRAY)
+ 
+    # Perform Otsu thresholding and extract the background.
+    # We use Binary Threshold as we want to create an all white background
+    ret,background = cv2.threshold(myimage_grey,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+ 
+    # Convert black and white back into 3 channel greyscale
+    background = cv2.cvtColor(background, cv2.COLOR_GRAY2BGR)
+ 
+    # Perform Otsu thresholding and extract the foreground.
+    # We use TOZERO_INV as we want to keep some details of the foregorund
+    ret,foreground = cv2.threshold(myimage_grey,0,255,cv2.THRESH_TOZERO_INV+cv2.THRESH_OTSU)  #Currently foreground is only a mask
+    foreground = cv2.bitwise_and(myimage,myimage, mask=foreground)  # Update foreground with bitwise_and to extract real foreground
+ 
+    # Combine the background and foreground to obtain our final image
+    finalimage = background+foreground
+ 
+    return finalimage
+
+def bgremove2(myimage):
+    # First Convert to Grayscale
+    myimage_grey = cv2.cvtColor(myimage, cv2.COLOR_BGR2GRAY)
+ 
+    ret,baseline = cv2.threshold(myimage_grey,127,255,cv2.THRESH_TRUNC)
+ 
+    ret,background = cv2.threshold(baseline,126,255,cv2.THRESH_BINARY)
+ 
+    ret,foreground = cv2.threshold(baseline,126,255,cv2.THRESH_BINARY_INV)
+ 
+    foreground = cv2.bitwise_and(myimage,myimage, mask=foreground)  # Update foreground with bitwise_and to extract real foreground
+ 
+    # Convert black and white back into 3 channel greyscale
+    background = cv2.cvtColor(background, cv2.COLOR_GRAY2BGR)
+ 
+    # Combine the background and foreground to obtain our final image
+    finalimage = background+foreground
+    return finalimage
+
+def bgremove3(myimage):
+    # BG Remover 3
+    myimage_hsv = cv2.cvtColor(myimage, cv2.COLOR_BGR2HSV)
+     
+    #Take S and remove any value that is less than half
+    s = myimage_hsv[:,:,1]
+    s = np.where(s < 127, 0, 1) # Any value below 127 will be excluded
+ 
+    # We increase the brightness of the image and then mod by 255
+    v = (myimage_hsv[:,:,2] + 127) % 255
+    v = np.where(v > 127, 1, 0)  # Any value above 127 will be part of our mask
+ 
+    # Combine our two masks based on S and V into a single "Foreground"
+    foreground = np.where(s+v > 0, 1, 0).astype(np.uint8)  #Casting back into 8bit integer
+ 
+    background = np.where(foreground==0,255,0).astype(np.uint8) # Invert foreground to get background in uint8
+    background = cv2.cvtColor(background, cv2.COLOR_GRAY2BGR)  # Convert background back into BGR space
+    foreground=cv2.bitwise_and(myimage,myimage,mask=foreground) # Apply our foreground map to original image
+    finalimage = background+foreground # Combine foreground and background
+ 
+    return finalimage
+
+
+image_path = r"C:\Users\VolggerF\Pictures\Camera Roll\WIN_20250130_13_58_50_Pro.jpg"
+frame = cv2.imread(image_path)
+
+frame=bgremove3(frame)
+showimage(frame)
